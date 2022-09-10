d55945baff6f1814efddde80f17d4fd405cb9b81
Ertuğrul Bilgiç <bilgicertugrul6@gmail.com>
2022-09-10 16:04:36 +0300

    start_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app_ankara_flutter/start_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      home:  Scaffold(
+
+        body: StartScreen(),
       ),
     );
   }
