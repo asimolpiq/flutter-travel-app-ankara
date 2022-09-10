32e70dcf35171b179d24390e94cb113e16098ba1
asimolpiq <alpayguroglu@gmail.com>
2022-09-10 14:37:34 +0300

    theme light

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Travel App',
-      theme: ThemeData.dark().copyWith(appBarTheme: AppBarTheme(centerTitle: true)),
+      theme: ThemeData.light().copyWith(appBarTheme: AppBarTheme(centerTitle: true)),
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
