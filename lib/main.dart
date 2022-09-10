d2078c7171142d7436386073a603e0590451e338
asimolpiq <alpayguroglu@gmail.com>
2022-09-10 14:29:41 +0300

    main dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app_ankara_flutter/view/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Travel App', home: HomeScreen());
   }
 }
