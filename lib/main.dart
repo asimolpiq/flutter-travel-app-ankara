5db644b2ca9c5048f5de4369d351fa1d94465daa
necatichdar <necaticuhadar06@gmail.com>
2022-09-10 15:42:26 +0300

    login view finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app_ankara_flutter/views/login_vew.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: LoginView(),
+      color:  const Color(0xff0164FF),
     );
   }
 }
